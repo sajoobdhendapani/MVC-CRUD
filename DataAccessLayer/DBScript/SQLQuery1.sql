4645d457278941c5d805b70e960bba93b62cd950
sajoob <sajoobdhendapani@gmail.com>
2024-01-17 13:07:07 +0530

    fff

--- DataAccessLayer/DBScript/SQLQuery1.sql
+++ DataAccessLayer/DBScript/SQLQuery1.sql
@@ -94,3 +94,14 @@ select* from Locations
 end
 drop procedure selectLocation
 exec selectLocation
+
+select * from Registration
+
+create procedure
+InsertRegister
+(@Username nvarchar(max),@Password nvarchar(max))
+as 
+begin
+insert into Registration values(@Username,@Password)
+end
+exec InsertRegister 'sajoob' , '1234'
